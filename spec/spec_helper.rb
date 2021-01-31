7aa1fe87c03ef87b062735f5fed776874dc0e3ae
Toshiki <toshiki.dev@protonmail.ch>
2021-01-31 16:20:31 +0900

    Generate template by 'bundle gem --exe --coc --mit --test=rspec --ci=github -- .' 'feature/template'

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "find_the_end"
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
